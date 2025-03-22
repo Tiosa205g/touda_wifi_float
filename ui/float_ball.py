310d815e89e1e592c11c89dd60ff74a0ac2e4c4c
Tiosa <2546168529@qq.com>
2025-03-22 18:54:54 +0800

    水球组件加入背景以及文字显示

--- ui/float_ball.py
+++ ui/float_ball.py
@@ -1,28 +1,29 @@
-from PySide6.QtWidgets import QApplication,QHBoxLayout
-from PySide6 import QtCore
+from PySide6.QtWidgets import QApplication, QHBoxLayout
+from PySide6 import QtCore, QtGui
 from PySide6.QtGui import QIcon
 from components import WaterBall
 from windows import DragWindow
 from tray import Tray
+
 class FloatBall(DragWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         self.setFixedSize(1000, 700)
         self.initUI()
-
         self.show()
+
     def initUI(self):
         mainLayout = QHBoxLayout(self)
         mainLayout.setContentsMargins(0, 0, 0, 0)
 
         self.waterball = WaterBall(100)
-        self.waterball.setFixedSize(100,100)
+        self.waterball.setFixedSize(100, 100)
         self.waterball.progress = 50
         mainLayout.addWidget(self.waterball)
         self.setLayout(mainLayout)
 
-        
 if __name__ == '__main__':
     import sys
 
@@ -30,6 +31,5 @@ if __name__ == '__main__':
     tray = Tray(QIcon('D:\\Source\\Python\\touda_wifi_float\\res\\favicon.ico'))
     win = FloatBall()
     win.show()
-    
 
     app.exec()
