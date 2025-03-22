c08d21f63e96c0638e20c5333290d581b4738d00
Tiosa <2546168529@qq.com>
2025-03-22 17:34:05 +0800

    修复托盘问题

--- ui/tray.py
+++ ui/tray.py
@@ -7,20 +7,20 @@ class Tray(QSystemTrayIcon):
         self.setToolTip('Tray')
 
         self.menu = QMenu()
-        self.showAction = QAction('Show', self)
+        self.displayAction = QAction('Display', self)
         self.quitAction = QAction('Quit', self)
 
-        self.showAction.triggered.connect(self.show)
+        self.displayAction.triggered.connect(self.display)
         self.quitAction.triggered.connect(self.quit)
 
-        self.menu.addAction(self.showAction)
+        self.menu.addAction(self.displayAction)
         self.menu.addAction(self.quitAction)
 
         self.setContextMenu(self.menu)
         print('Tray created')
         self.show()
 
-    def show(self):
+    def display(self):
         print('show')
 
     def quit(self):
