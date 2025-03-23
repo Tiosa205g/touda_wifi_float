8600e6be73113461b1328f95174bfa582a9b1951
Tiosa <2546168529@qq.com>
2025-03-23 21:46:07 +0800

    细分资源文件位置，删除悬浮窗多余的控制按钮

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from PySide6 import QtCore
-from qframelesswindow import FramelessWindow
-from ui import WaterBall
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+from ui import FloatBall
+from ui import Tray
+if __name__ == '__main__':
+    app = QApplication()
+    tray = Tray(QIcon('res/ico/favicon.ico'))
+    win = FloatBall()
+    win.show()
+
+    app.exec()
--- ui/__init__.py
+++ ui/__init__.py
@@ -1,2 +1,4 @@
 from .components import *
+from .float_ball import FloatBall
+from .tray import Tray
 #导入组件
