133356329c5c6c3ad7721c425ea8240b7a830de9
Tiosa <2546168529@qq.com>
2025-03-26 00:08:12 +0800

    添加悬浮球右键菜单

--- src/win_float_ball.py
+++ src/win_float_ball.py
@@ -1,4 +1,4 @@
-from qfluentwidgets import TeachingTip, TeachingTipTailPosition
+from qfluentwidgets import TeachingTip, TeachingTipTailPosition, RoundMenu, Action
 from qfluentwidgets import FluentIcon as FIF
 from ui.float_ball import UI_FloatBall
 from ui.windows.drag_window import DragWindow
@@ -10,8 +10,9 @@ class FloatBall(DragWindow):
         self.setFixedSize(self.ui.waterball.size())
 
         self.setCanLeftScreen(False, screen_size=screen_size)
-
+        self.ui.waterball.contextMenuEvent = self.waterball_menu
         self.ui.waterball.clicked.connect(self.waterball_clicked)
+        #self.ui.waterball.rightClicked.connect(self.waterball_right_clicked)
     def waterball_clicked(self):
         TeachingTip.create(self.ui.waterball,
                             'This is a ball','This is a ball that you can click on',
@@ -19,3 +20,20 @@ class FloatBall(DragWindow):
                             isClosable=True,
                             duration=3000,
                             parent=self.ui.waterball)
+    def waterball_menu(self,e):
+        mainMenu = RoundMenu()
+        accountMenu = RoundMenu("账号")
+        linkMenu = RoundMenu("链接")
+
+        accountMenu.addActions([Action("切换到账号1", icon=FIF.CHAT, triggered=lambda: print("切换账号1")),
+                                Action("切换到账号2", icon=FIF.CHAT, triggered=lambda: print("切换账号2")),
+                                Action("切换到账号3", icon=FIF.CHAT, triggered=lambda: print("切换账号3"))])
+        
+        linkMenu.addActions([Action("链接1", icon=FIF.LINK, triggered=lambda: print("链接1")),
+                             Action("链接2", icon=FIF.LINK, triggered=lambda: print("链接2")),
+                             Action("链接3", icon=FIF.LINK, triggered=lambda: print("链接3"))])
+        
+        mainMenu.addMenu(accountMenu)
+        mainMenu.addMenu(linkMenu)
+
+        mainMenu.exec(e.globalPos())
--- ui/float_ball.py
+++ ui/float_ball.py
@@ -4,9 +4,10 @@ from .components import WaterBall
 
 class UI_FloatBall:
     def setupUI(self, FloatBall):
-        FloatBall.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint | Qt.Tool)
-        FloatBall.titleBar.hide()
-        FloatBall.setAttribute(Qt.WA_TranslucentBackground)
+        FloatBall.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint | Qt.Tool) # 窗口置顶, 无边框, 隐藏任务栏
+        FloatBall.titleBar.hide() # 隐藏标题栏(最小化, 最大化, 关闭)
+        FloatBall.setAttribute(Qt.WA_TranslucentBackground) # 设置窗口透明
+        
         #self.setFixedSize(1000, 700)
         self.mainLayout = QHBoxLayout(FloatBall)
         self.mainLayout.setContentsMargins(0, 0, 0, 0)
