93af7db5c044047e8381f389299b4c6770cfebdb
Tiosa <2546168529@qq.com>
2025-03-26 11:40:10 +0800

    优化显示隐藏

--- src/tray.py
+++ src/tray.py
@@ -5,20 +5,24 @@ class Tray(QSystemTrayIcon):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.setIcon(parent.windowIcon())
-        self.setToolTip('Tray')
+        self.setToolTip('Touda WiFi')
 
         self.menu = SystemTrayMenu("touda_wifi", parent=parent)
-        self.menu.addActions([Action('Display',triggered=self.display),
-                              Action('Quit', triggered=self.quit)])
+        self.menu.addActions([Action('退出', triggered=self.quit)])
+        self.activated.connect(self.toggle)
 
         self.setContextMenu(self.menu)
         self.show()
 
         print('Tray created')
 
-    def display(self):
-        print('show')
+    def toggle(self, reason):
+        
+        if reason == QSystemTrayIcon.ActivationReason.Trigger:
+            print('toggle')
+            self.parent().setHidden(not self.parent().isHidden())
 
     def quit(self):
         print('quit')
         exit()
+    
--- src/win_float_ball.py
+++ src/win_float_ball.py
@@ -29,11 +29,16 @@ class FloatBall(DragWindow):
                                 Action("切换到账号2", icon=FIF.CHAT, triggered=lambda: print("切换账号2")),
                                 Action("切换到账号3", icon=FIF.CHAT, triggered=lambda: print("切换账号3"))])
         
-        linkMenu.addActions([Action("链接1", icon=FIF.LINK, triggered=lambda: print("链接1")),
+        linkMenu.addActions([Action("打开剪贴板链接", icon=FIF.LINK, triggered=lambda: print("打开剪贴板链接")),
+                             Action("链接1", icon=FIF.LINK, triggered=lambda: print("链接1")),
                              Action("链接2", icon=FIF.LINK, triggered=lambda: print("链接2")),
                              Action("链接3", icon=FIF.LINK, triggered=lambda: print("链接3"))])
+        # 链接内还应加入子菜单选择类别，以及是否使用webvpn打开
         
         mainMenu.addMenu(accountMenu)
         mainMenu.addMenu(linkMenu)
+        mainMenu.addSeparator()
+        mainMenu.addActions([Action("隐藏", icon=FIF.HIDE, triggered=lambda: self.setHidden(True)),
+                             Action("退出", icon=FIF.CLOSE, triggered=lambda: exit())])
 
         mainMenu.exec(e.globalPos())
