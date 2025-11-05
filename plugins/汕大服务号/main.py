40d8c14a8d39b0910e8e75403121baf6165f5aaa
Tiosa <2546168529@qq.com>
2025-11-06 00:26:42 +0800

    大致完成汕大服务号绑定

--- plugins/汕大服务号/main.py
+++ plugins/汕大服务号/main.py
@@ -1,7 +1,12 @@
-# 这是一个示例插件
+
+import base64
 import pluggy
 import requests
+from bs4 import BeautifulSoup
 from pathlib import Path
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtCore import QUrl
+from PySide6.QtWidgets import QWidget,QVBoxLayout
 
 PLUGIN_NAME = '汕大服务号插件'
 PLUGIN_VERSION = '1.0.0'
@@ -18,7 +23,15 @@ class SDK:
         self.api.logger.info(f'[{PLUGIN_NAME}] {msg}')
     def logger_error(self,msg:str):
         self.api.logger.error(f'[{PLUGIN_NAME}] {msg}')
-    
+    def get_sub(self)->dict: #{'username':xxx,'pwd':xxx}
+        return {'username':self.api.wifi.username,'pwd':self.api.wifi.password}
+    def get_current_account(self)->dict:
+        """{'name':xxx,'pwd':xxx} 不一定已经登录，比如到了临时用户时间"""
+        main = self.api.cfg(self.api.MAIN_CFG)
+        current = self.api.cfg(f"{self.api.CFG_DIR}\\account_{main.get('main','current_account',0)}.toml")
+        name = current.get('setting','name','')
+        password = base64.b64decode(current.get('setting','password','').encode('utf-8')).decode('utf-8')
+        return {'name':name,'pwd':password}
     class Menu: # 插件名 功能名 函数
         def __init__(self):
             self.menu = []
@@ -33,8 +46,8 @@ class SDK:
 
         def del_func(self,func_name:str)->bool:
             """删除指定功能名的功能"""
-            num = len(num)
-            self.menu = [x for x in self.menu if x['function'] != func_name] 
+            num = len(self.menu)
+            self.menu = [x for x in self.menu if x['function'] != func_name]
             return len(self.menu) != num
         
         def get_all(self)->list[dict]:
@@ -45,7 +58,11 @@ class Plugin:  # 类名固定为 Plugin，不能变动，否则无法识别
         """插件加载时执行，传api包含已实例化的wifi和webvpn类"""
         self.api = api
         self.sdk = SDK(self.api)
-        self.sdk.logger_info(f'当前wifi用户名：{self.api.wifi.name}')
+        self.session = requests.session()
+        self.session.trust_env = True
+        self.session.verify = False
+        self.session.headers.update({'User-Agent':'Mozilla/5.0 (Linux; Android 14; ALP-AN00 Build/HONORALP-AN00T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/138.0.7204.180 Mobile Safari/537.36 XWEB/1380267 MMWEBSDK/20250804 MMWEBID/6729 MicroMessenger/8.0.63.2920(0x28003F3C) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64',
+                                     'Content-Type':'application/x-www-form-urlencoded'})
         return True # 返回true才会视为有效插件
     @hook
     def on_disable(self):
@@ -66,20 +83,97 @@ class Plugin:  # 类名固定为 Plugin，不能变动，否则无法识别
     def get_menu(self)->list[dict]: # list[功能 - function]
         """获取插件的菜单信息,需要返回list[{'function':'功能名','object':callable函数}]"""
         menu = self.sdk.Menu()
-        menu.add_func('获取一卡通余额',self.hello_world)
+        acc = self.sdk.get_current_account()
+        name = acc['name']
+        pwd = acc['pwd']
+        openid = self.get_encode_openid(name)
+        if (self.is_login(name)):
+            url_card = f'http://wechat.stu.edu.cn/wechat/smartcard/index?openid={openid}'
+            url_net = f'http://wechat.stu.edu.cn/wechat/stu_netms_service/stu_netms.aspx?openid={openid}'
+            menu.add_funcs([
+                {'function':'进入一卡通页面',
+                 'object': (lambda *_, url=url_card: self.open_browser(url,'一卡通'))},
+                {'function':'进入网络服务页面',
+                 'object': (lambda *_, url=url_net: self.open_browser(url,'网络服务'))},
+                {'function':'解绑',
+                 'object': (lambda *_, name=name: self.unbind_wechat(name))}
+            ])
+
+        else:
+            menu.add_func('绑定账号', lambda *_, name=name, pwd=pwd: self.auth_wechat(name,pwd))
         return menu.get_all()
     
-    def query_card_remain(self):
-        self.sdk.logger_info('hello world')
+    def get_encode_openid(self,name:str)->str:
+        """获取base64编码后的用户名，并去除等号，用于伪造一个openid并确保安全问题"""
+        if not name:
+            self.sdk.logger_error('非法用户名')
+            return ''
+        return base64.urlsafe_b64encode(name.encode()).decode('utf-8').rstrip('=')
+    def is_login(self,name:str)->bool:
+        """判断是否登录"""
+        open_id = self.get_encode_openid(name)
+        r = self.session.get(url=f'http://wechat.stu.edu.cn/wechat/login/login?source_type=dorm_information&openid={open_id}')
+        if r.status_code == 200:
+            return r.text.find('登录') == -1 # 寻找是否有 登录 字样，如果未绑定就会有登录提示
+        return False               
+    def auth_wechat(self,name:str,pwd:str)->bool:
+        """使用伪造openid进行登录并绑定"""
+        open_id = self.get_encode_openid(name)
+        r = self.session.post(url='http://wechat.stu.edu.cn/wechat/login/login_verify',data=f'ldap_account={name}&ldap_password={pwd}&btn_ok=登录&source_type=dorm_information&openid={open_id}')
+        if r.status_code == 200:
+            return r.text.find('成功') != -1
+        return False 
+    def unbind_wechat(self,name:str)->bool:
+        """将伪造openid取消绑定"""
+        open_id = self.get_encode_openid(name)
+        r = self.session.get(url=f'http://wechat.stu.edu.cn/wechat/ldap_openid_ok.aspx?openid={open_id}')
+        if r.status_code != 200:
+            return False
+        
+        soup = BeautifulSoup(r.text,'lxml')
 
-    def query_card_records(self):
-        pass
-    
-    def apply_webvpn(self):
-        pass
+        __VIEWSTATE = soup.find('input',attrs={'type':'hidden','name':'__VIEWSTATE'}).get('value')
+        __EVENTVALIDATION = soup.find('input',attrs={'type':'hidden','name':'__EVENTVALIDATION'}).get('value')
 
-    def unbind_totp(self):
-        pass
+        r = self.session.post(url=f'http://wechat.stu.edu.cn/wechat/ldap_openid_ok.aspx?openid={open_id}',data=f'__VIEWSTATE={__VIEWSTATE}&__EVENTVALIDATION={__EVENTVALIDATION}&Btn_cancel_bind=')
+        if r.status_code == 200:
+            return r.text.find('取消绑定成功') != -1
+        return False
+    def open_browser(self,url:str,title:str):
+        self.browser = SimpleBrowser(url,title)
+        self.browser.show()
+class SimpleBrowser(QWidget):
+    def __init__(self,url:str,title:str):
+        super().__init__()
+        self.title = title
+        self.url = url
+        self.init_ui()
+        
+    def init_ui(self):
+        # 设置窗口基本属性
+        self.setWindowTitle(self.title)
+        self.setGeometry(100, 100, 500,500)  # x, y, 宽, 高
+        
+        # 创建布局管理器
+        layout = QVBoxLayout()
+        self.setLayout(layout)  # 直接给当前QWidget设置布局
+        
+        # 创建浏览器组件
+        self.web_view = QWebEngineView()
+        
+        # 添加浏览器到布局
+        layout.addWidget(self.web_view)
+        
+        # 加载网页
+        self.web_view.load(QUrl(self.url))
+        
+        # 显示加载进度
+        self.web_view.loadProgress.connect(self.update_progress)
+        
+    def update_progress(self, progress):
+        if progress < 100:
+            self.setWindowTitle(f"{self.title} - 加载中: {progress}%")
+        else:
+            self.setWindowTitle(f"{self.title} - 加载完成")
+
     
-    def bind_mac(self):
-        pass
